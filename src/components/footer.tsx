8760892607bf552cf76d8abe600d8c0ea80b803d
Dan Cargill <dan@cargill.dev>
2025-01-30 00:36:37 +0000

    Add metadata

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,9 @@ import Counter from './counter'
 export default function Footer() {
   return (
     <footer className="text-foreground-muted fixed right-0 bottom-0 flex w-full items-end justify-between p-4">
-      <Counter />
+      <div>
+        <Counter />
+      </div>
       <Link href="https://cargill.dev" className="text-[10px] underline">
         Dan Cargill
       </Link>
